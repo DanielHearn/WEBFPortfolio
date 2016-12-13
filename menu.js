50a2e80ba48b49530d343c6c862cfa90a3dbb8ad
DanielHearn <hearndaniel@live.co.uk>
2016-12-13 22:53:04 +0000

    Add mobile menu

---
+++ menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("overlay").style.height = "100vh";
+    document.getElementById("overlay-content").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("overlay").style.height = "0vh";
+    document.getElementById("overlay-content").style.display = "none";
+}
