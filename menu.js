4e410742fd1e36c9e31aa6c6a1b5e20f445eae29
DanielHearn <hearndaniel@live.co.uk>
2016-12-27 19:43:51 +0000

    Add new version

--- menu.js
+++ menu.js
@@ -1,3 +1,18 @@
+var mainbottom = $('#main').offset().top + $('#main').height();
+
+// on scroll,
+$(window).on('scroll',function(){
+
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+        $('.nav').addClass('past-main');
+    } else {
+        $('.nav').removeClass('past-main');
+   }
+
+});
+
 function nav() {
    document.getElementById("overlay").classList.toggle("active");
    document.getElementById("overlay-button").classList.toggle("active");
