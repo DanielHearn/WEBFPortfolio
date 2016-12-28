c772be590340c381cf5cfb9924b747f4889dd2f2
DanielHearn <hearndaniel@live.co.uk>
2016-12-28 16:23:12 +0000

    Fix background image animation with IE

--- menu.js
+++ menu.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   });
 
   function scrollToAnchor(aid){
-    var aTag = $("a[name='"+ aid +"']");
+    var aTag = $("a[id='"+ aid +"']");
     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
   }
 
