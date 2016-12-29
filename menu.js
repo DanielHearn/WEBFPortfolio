ef641164ca0bda9b48d0aa58deaeb16a7bf2af60
DanielHearn <hearndaniel@live.co.uk>
2016-12-29 19:31:12 +0000

    Add new version

--- menu.js
+++ menu.js
@@ -1,29 +1,12 @@
 $(document).ready(function () {
+  
   $("#overlay-button").click(function() {
     $(this).toggleClass('active');
     $("#overlay").toggleClass('active');
   });
 
-  function scrollToAnchor(aid){
-    var aTag = $("a[id='"+ aid +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-  }
-
   $("#view-about").click(function() {
-      scrollToAnchor('about');
+    $('html,body').animate({scrollTop: $("#about").offset().top},'slow');
   });
 
-  var navbottom = $('nav').offset().top;
-  var mainbottom = $('main').offset().top - 100;
-
-  $(window).on('scroll',function(){
-      var stop = Math.round($(window).scrollTop());
-
-      if (stop > mainbottom) {
-          $('nav').addClass('past-main');
-      } else {
-          $('nav').removeClass('past-main');
-      }
-
-  });
 });
