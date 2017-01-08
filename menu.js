ebd46e81b1a75173a51a11dc2deb47983e5abd22
DanielHearn <hearndaniel@live.co.uk>
2017-01-08 13:13:06 +0000

    Change js features to css

--- menu.js
+++ menu.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-  
-  $("#overlay-button").click(function() {
-    $(this).toggleClass('active');
-    $("#overlay").toggleClass('active');
-  });
 
   $("#view-about").click(function() {
     $('html,body').animate({scrollTop: $("#about").offset().top},'slow');
